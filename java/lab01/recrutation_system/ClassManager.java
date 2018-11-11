939d386d0da45de213d70a908be210003bf5036e
sagan.pawel1000@gmail.com <sagan.pawel1000@gmail.com>
2018-11-11 09:54:45 +0100

    Compare students by points function some modifications. From this time students with much amount of points are remach to have best priority configuration

--- java/lab01/recrutation_system/ClassManager.java
+++ java/lab01/recrutation_system/ClassManager.java
@@ -167,9 +167,11 @@ class ClassManager {
                 // first sort students by point, and than fill school with unbind students
             case 2:
                 this.divideByPoints();
-                this.fillSchoolWithUnbindStudents();
                 break;
         }
+        if(availableStudents.size() != 0)
+            this.fillSchoolWithUnbindStudents();
+
         return true;
     }
 
@@ -195,12 +197,45 @@ class ClassManager {
             return;
 
         this.availableStudents.sort(Student::compare);
+        // if user want to reorder students to get best match
+        if(this.Priorities[1].getisActive())
+            // sort subgroups
+            this.groupAndSortByPriorities();
         this.moveToSchool();
+    }
+    // function grouping students who have same points quantity
+    // than they are sorting by preferences
+    private void groupAndSortByPriorities() {
+        ArrayList< ArrayList<Student> > studentsGroupedByPoints = new ArrayList<>() ;
+        Integer lastPointsAmount = -1;
+        int i = -1;
+        // create small subgroups of students with same preferences
+        for (Student student : this.availableStudents) {
+            // create new subgroup when last student has different
+            // preference array
+            if( !(lastPointsAmount.intValue() ==  student.getpoints()) ) {
+                i++;
+                lastPointsAmount = student.getpoints();
+                studentsGroupedByPoints.add( new ArrayList<>());
+            }
+            studentsGroupedByPoints.get(i).add(student);
+        }
+        // create new array for sorted students
+        this.availableStudents = new ArrayList<>();
+
+        // for every subgroup sort students by points
+        // add sorted students to table
+        for (ArrayList<Student> studentGroup : studentsGroupedByPoints) {
+            // sort
+            studentGroup.sort(Student::comparePrioritySize);
+            // add each subgroup to availableStudents array
+            availableStudents.addAll(studentGroup);
+        }
     }
 
     // function grouping students who have same preferences
     // than they are sorting by points
-    private void groupAndSort() {
+    private void groupAndSortByPoints() {
         ArrayList< ArrayList<Student> > studentsGroupedByPreferences = new ArrayList<>() ;
         ArrayList<Integer> lastPreference = new ArrayList<Integer>(0);
         int i = -1;
@@ -238,7 +273,7 @@ class ClassManager {
         // if user want to reorder students to get best match
         if(this.Priorities[1].getisActive())
             // sort subgroups
-            this.groupAndSort();
+            this.groupAndSortByPoints();
         // move all students to proper schools
         this.moveToSchool();
     }
